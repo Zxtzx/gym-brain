9644c768697a282cc8e4a53217ead6aa827ade0e
Zxtzx <declan.dufour@gmail.com>
2026-06-03 22:37:54 -0400

    Create gym0.2.py

---
+++ gym0.2.py
@@ -0,0 +1,66 @@
+def get_num(question):
+    while True:
+        try:
+            number = float(input(question))
+            return number
+        except ValueError:
+            print("Please enter a valid number")
+
+def get_exercise():
+    while True:
+            exercise = input("Please enter what exercise you have done: ")
+
+            if exercise.isalpha():
+                return exercise
+                
+            else:
+               print("Please enter only letters!")
+
+            
+def get_calc(weight, reps):
+    epely = weight * (1 + reps / 30)
+    return epely
+while True:
+    exercise = get_exercise()
+    sets = int(get_num(f"How many sets did you do for {exercise}?: "))
+    volume = 0
+    best = 0
+    for i in range(sets):
+        print(f"Set {i + 1}")
+        weight = get_num(f"What weight did you do for set {i + 1} of {exercise}?: ")
+        reps = get_num(f"How many reps of {weight:.0f} lbs did you do for Set {i + 1} of {exercise}?: ")
+        
+        epely = get_calc(weight, reps)
+        if epely > best:
+            best = epely
+
+        
+        set_volume = (weight * reps)
+        volume = set_volume + volume
+
+    print(f"Your estimated 1RM for {exercise} is {best:.0f}!")
+    print(f"Your total volume for {sets} sets of {exercise} is {volume:.0f} lbs!")
+            
+    again = input("Would you like to calculate another exercise? (Yes/No): ").lower()
+    if again == "no":
+        print("Thank you")
+        quit()
+    elif again == "yes":
+        continue
+    else:
+        print("Please enter either Yes or No")
+        
+        
+                
+
+        
+        
+
+
+
+
+
+
+
+
+        
